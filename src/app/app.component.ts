fbe71752fc63a32d64c228224aa24c8155139272
gustavo <cetres@gmail.com>
2017-12-03 01:13:38 -0200

    problema do ionic-native antigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Splashscreen } from '@ionic-native/splash-screen';
 
 import { DataProvider } from '../providers/data';
 import { AuthProvider } from '../providers/auth';
